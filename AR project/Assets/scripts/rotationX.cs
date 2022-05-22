12537a1ad6718dc93d5553fe93ea546a8f629f82
Pgiannako <43864555+Pgiannako@users.noreply.github.com>
2022-05-23 00:08:36 +0300

    version 2.1
    Refactored project folders
    added youtube logo to each image

---
+++ AR project/Assets/scripts/rotationX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationX : MonoBehaviour
+{
+    private float xAcc;
+    //private GameObject invisibleCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xAcc = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * xAcc, 0f, 0f );
+        //xAngle += 1;
+    }
+}
