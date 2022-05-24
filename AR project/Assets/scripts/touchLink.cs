b9b545e92c9e0a938939738e9cf1059a6a42cf72
PBegkas <cs03286@uoi.gr>
2022-05-24 18:34:13 +0300

    added wikipedia and object touch links

--- AR project/Assets/scripts/touchLink.cs
+++ AR project/Assets/scripts/touchLink.cs
@@ -13,9 +13,68 @@ public class touchLink : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.touchCount == 1)
         {
-            Application.OpenURL("https://en.wikipedia.org/wiki/Gabe_Newell");
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                if (hit.collider != null)
+                {
+                    GameObject touchedObject = hit.transform.gameObject;
+                    // wikipedia links
+                    if (touchedObject.tag == "gabe")
+                    {
+                        Application.OpenURL("https://en.wikipedia.org/wiki/Gabe_Newell");
+                    }
+                    else if(touchedObject.tag == "ronaldo")
+                    {
+                        Application.OpenURL("https://en.wikipedia.org/wiki/Cristiano_Ronaldo");
+                    }
+                    else if (touchedObject.tag == "lewis")
+                    {
+                        Application.OpenURL("https://en.wikipedia.org/wiki/Lewis_Hamilton");
+                    }
+                    else if (touchedObject.tag == "beyonce")
+                    {
+                        Application.OpenURL("https://en.wikipedia.org/wiki/Beyonc%C3%A9");
+                    }
+                    else if (touchedObject.tag == "john")
+                    {
+                        Application.OpenURL("https://en.wikipedia.org/wiki/John_Wick");
+                    }
+                    else if (touchedObject.tag == "freddie")
+                    {
+                        Application.OpenURL("https://en.wikipedia.org/wiki/Freddie_Mercury");
+                    }
+                    // object links
+                    else if (touchedObject.tag == "ball")
+                    {
+                        Application.OpenURL("https://www.cristianoronaldo.com/#cr7");
+                    }
+                    else if (touchedObject.tag == "valve")
+                    {
+                        Application.OpenURL("https://store.steampowered.com/");
+                    }
+                    else if (touchedObject.tag == "car")
+                    {
+                        Application.OpenURL("https://www.formula1.com/en/drivers/lewis-hamilton.html");
+                    }
+                    else if (touchedObject.tag == "gun")
+                    {
+                        Application.OpenURL("https://www.imdb.com/title/tt2911666/?ref_=fn_al_tt_1");
+                    }
+                    else if (touchedObject.tag == "micB")
+                    {
+                        Application.OpenURL("https://open.spotify.com/artist/6vWDO969PvNqNYHIOW5v0m = BEYONCE");
+                    }
+                    else if (touchedObject.tag == "micF")
+                    {
+                        Application.OpenURL("https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d =QUEEN");
+                    }
+
+                }
+            }
         }
     }
 }
